102afa740e32d952e97e8aed6a620400ab8ea84e
Daniel <1stloveyourself@gmail.com>
2017-05-30 15:07:37 -0600

    Oh Now

--- app/components/autha.js
+++ app/components/autha.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 class Auth extends Component {
   componentWillMount() {
     this.props.getQuery(this.props.location.query);
